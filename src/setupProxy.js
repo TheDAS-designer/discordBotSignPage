957f73eee8da44bca7ea86e6d8be434fae7ebfb3
smiley <consensusworld@protonmail.com>
2022-12-02 11:23:47 +0800

    123

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 import {createProxyMiddleware} from "http-proxy-middleware"
 
-export default function (app) {
+module.exports = function (app) {
     console.log("app:", app)
     target="https://bot.loopss.me"
     app.use('/ticket', createProxyMiddleware({
