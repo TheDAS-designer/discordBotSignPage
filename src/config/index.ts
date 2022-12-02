f760ee3bf1d654d2bbf7552f5690cce7378515b7
smiley <consensusworld@protonmail.com>
2022-12-02 11:16:12 +0800

    cos

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 
 // export const baseURL = process.env.NEXT_PUBLIC_BOT_URL;
-export const baseURL = "/bot";
+export const baseURL = "";
 // export const baseURL = 'http://13.229.143.112:3001';
 // export const ifrURL = 'http://localhost:3000/target=http://localhost:3001'
 // export const ifrURL = 'https://nfgate.io/target=https://nftrust.cc'
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 import {createProxyMiddleware} from "http-proxy-middleware"
 
 export default function (app) {
+    console.log("app:", app)
     target="https://bot.loopss.me"
     app.use('/ticket', createProxyMiddleware({
         target,
