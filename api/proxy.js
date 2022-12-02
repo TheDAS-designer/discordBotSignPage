2997008ed42fb46fe70b4f6e2c216453d4e08881
smiley <consensusworld@protonmail.com>
2022-12-02 10:47:08 +0800

    change

--- api/proxy.js
+++ api/proxy.js
@@ -3,6 +3,9 @@ import createProxyMiddleware from "http-proxy-middleware"
 export default function (req, res) {
     let target= ""
 
+    console.log("req:", req)
+    console.log("req.url:", req.url)
+
     if(req.url.startWith("/bot")){
         target="https://bot.loopss.me"
     }
